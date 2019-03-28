104a659b094c47ab11755ae1896abee0d14c2cdd
Matt Moretti <werebus@gmail.com>
2019-03-28 16:03:29 -0400

    Also bail early if `sudo` is already a function
    To prevent a double-run, which hangs becuase the script itself calls
    sudo. Also added some comments.

--- run.sh
+++ run.sh
@@ -5,16 +5,23 @@ set -euo pipefail
 adminuser="$USER-d"
 admingroup="admin"
 
+# If sudo is already a function, chances are we're double-running.
+[ "x$(type -t sudo)" = "xfunction" ] && exit 0
+
+# If the user can aready sudo, that's fine.
 if sudo -vn &>/dev/null; then
   exit 0
 fi
 
+# We have to rely on group membership here, to avoid password-prompting.
 until id -Gn "$adminuser" 2>/dev/null | grep "$admingroup" &>/dev/null; do
   read -rp 'Enter your admin username: ' adminuser
 done
 
+# Lie #1: sudo us actually a call to su now.
 function sudo {
   su "$adminuser" -c "sudo $*"
 }
 
+# Lie #2: groups claims the user is an admin even if they're not.
 alias groups="echo -n '$admingroup ' && groups"
