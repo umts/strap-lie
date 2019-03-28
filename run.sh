b51d60c8f6289ea23d5b5c3c83b5a363eaff088d
Matt Moretti <werebus@gmail.com>
2019-03-28 10:39:59 -0400

    Quieter group-checking

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 adminuser="$USER-d"
 admingroup="admin"
 
-until id -Gn "$adminuser" | grep "$admingroup"; do
+until id -Gn "$adminuser" > /dev/null 2>&1 | grep "$admingroup"; do
   read -rp 'Enter your admin username: ' adminuser
 done
 
